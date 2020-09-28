37f616e3e34b69511301cd5b127bddb5966e3fb1
Aurelien Gateau <mail@agateau.com>
2020-09-28 08:32:48 +0200

    Add header to rlt-changelog output

--- releasetools/rlt_changelog.py
+++ releasetools/rlt_changelog.py
@@ -5,17 +5,34 @@ Print the changes between the released version and now
 import argparse
 import sys
 
+from datetime import datetime
 from subprocess import run
 
 from releasetools.app import App
-from releasetools import tagtools
+from releasetools import tagtools, versiontools
+
+
+HEADER_TMPL = "## {version} - {date}\n"
+
+
+def print_header(app):
+    version = versiontools.read(app)
+    date = datetime.now().strftime("%Y-%m-%d")
+    print(HEADER_TMPL.format(version=version, date=date))
+
+
+def print_changelog(tag, git_log_options):
+    proc = run(["git", "log", git_log_options, f"{tag}..HEAD"],
+               capture_output=True, text=True)
+    print(str(proc.stdout))
 
 
 def app_main(app, args):
     tag = tagtools.get_release_tag()
     git_log_options = app.config["changelog"]["git_log_options"]
-    run(["git", "log", git_log_options, f"{tag}..HEAD"])
 
+    print_header(app)
+    print_changelog(tag, git_log_options)
     return 0
 
 
